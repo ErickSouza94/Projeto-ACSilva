c90528b5cb6c374752785de31589a198e8667583
ErickSouza94 <erick.ssouza94@gmail.com>
2026-04-05 02:10:26 +0100

    feat: garantir fallback para baseURL no axios

--- projeto-acsilva/src/components/Services/api.js
+++ projeto-acsilva/src/components/Services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // O endereço onde o NestJS está a correr
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL || 'https://acsilva-backend-render.onrender.com',
     headers: {
     'Content-Type': 'application/json',
   },
